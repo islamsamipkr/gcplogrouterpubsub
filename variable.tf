418673542daa39036964e44924d7b13bcff0908a
Sami Islam <islam.samipkr@gmail.com>
2024-06-02 00:39:46 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -8,14 +8,7 @@ variable "pubsub_config" {
 }
 
 # Example value
-pubsub_config = [
-  "example-config-1" = {
-    pubsub_topic_name          = "app_notifications"
-    pubsub_topic_labels        = { "env" = "dev" }
-    subscription_labels        = { "team" = "analytics" }
-    pubsub_subscription_name   = "example-subscription-1"
-  }
-]
+
 
 variable "source_project_name"{
 type=string
